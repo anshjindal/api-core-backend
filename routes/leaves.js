a9a9c1de82bc199ef65f84e8f35e5504753e2871
= <aalna096@uottawa.ca>
2025-03-17 15:19:24 -0400

    Post and Get Request

--- routes/leaves.js
+++ routes/leaves.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const leavesController = require("../controllers/leavesController");
+const verifySession = require("../middlewares/authenticationMiddleware");
 
-router.post("/:empId", leavesController.saveLeave);
-router.put("/:empId", leavesController.updateLeave);
-router.get("/:empId", leavesController.getAllLeaves);
+router.post("/:empId", verifySession, leavesController.saveLeave);
+router.put("/:empId", verifySession, leavesController.updateLeave);
+router.get("/:empId", verifySession, leavesController.getAllLeaves);
 
 module.exports = router;
