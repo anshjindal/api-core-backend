546f420877a712049f6254dc70e687a0bb430d5a
troyodia <odiatroy@gmail.com>
2025-03-26 18:49:06 -0400

    feat(backend): created route for getting an employees timesheet

--- controllers/timesheetController.js
+++ controllers/timesheetController.js
@@ -8,10 +8,23 @@ const createTimesheet = async (req, res) => {
     console.log(error.message);
     res
       .status(500)
-      .json({ error: "Failed to create timesheet:" + error.message });
+      .json({ error: "Failed to create timesheet. " + error.message });
+  }
+};
+const getEmployeeTimesheet = async (req, res) => {
+  const { empId } = req.params;
+  try {
+    const response = "";
+    res.status(201).json(response);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      error: `Failed to get timesheet for employee ${empId}. ` + error.message,
+    });
   }
 };
 
 module.exports = {
   createTimesheet,
+  getEmployeeTimesheet,
 };
--- routes/timesheetRoutes.js
+++ routes/timesheetRoutes.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
-const { createTimesheet } = require("../controllers/timesheetController");
+const {
+  createTimesheet,
+  getEmployeeTimesheet,
+} = require("../controllers/timesheetController");
 const verifySession = require("../middlewares/authenticationMiddleware");
 
 router.route("/createTimesheet").post(verifySession, createTimesheet);
+router.route("/:empId").get(verifySession, getEmployeeTimesheet);
 
 module.exports = router;
