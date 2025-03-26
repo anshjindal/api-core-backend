f8f390e02c8cafcd5f638bac81de46a3badb9f6a
troyodia <odiatroy@gmail.com>
2025-03-26 16:52:22 -0400

    feat(backend): added validation to timesheet schema

--- models/timesheet.js
+++ models/timesheet.js
@@ -1,26 +1,29 @@
 const mongoose = require("mongoose");
 
-const TimesheetSchema = new mongoose.Schema({
-  task: {
-    type: String,
-    required: true,
-    trim: true,
+const TimesheetSchema = new mongoose.Schema(
+  {
+    task: {
+      type: String,
+      required: [true, "Task done required"],
+      trim: true,
+    },
+    hoursWorked: {
+      type: Number,
+      required: [true, "Hours worked required"],
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["Approved", "Pending", "Rejected"],
+      default: "Pending",
+    },
+    date: {
+      type: String,
+      required: [true, "Work date required"],
+      trim: true,
+    },
   },
-  hoursWorked: {
-    type: Number,
-    required: true,
-    trim: true,
-  },
-  status: {
-    type: String,
-    enum: ["Approved", "Pending", "Rejected"],
-    default: "Pending",
-  },
-  date: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("TimesheetSchema", TimesheetSchema);
