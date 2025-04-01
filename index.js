82986a1afee7d4544dc2c3cc21aa15bf13617dbf
Suluseahawks <145396681+Suluseahawks@users.noreply.github.com>
2025-04-01 18:42:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,62 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
 const newsletterRoute = require("./routes/newsletterRoutes");
 const blogRoute = require("./routes/blog");
 const contactRoute = require("./routes/contact");
 const employeeRoutes = require("./routes/employeeRoutes");
-const departmentRoutes = require("./routes/departmentRoutes")
-const designationRoutes = require("./routes/designationRoutes")
+const departmentRoutes = require("./routes/departmentRoutes");
+const designationRoutes = require("./routes/designationRoutes");
 const taskRoutes = require("./routes/taskRoutes");
-
-
 const authRoutes = require("./routes/authenticationRoutes");
 const roleRoutes = require("./routes/roleRoutes");
+const settingRoutes = require("./routes/settingRoutes"); 
 
 const multer = require("multer");
-
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 require('dotenv').config({ path: "./.env" });
 
 const connectToDB = require("./utils/database");
 
-//new addon requires
-const cookieParser = require("cookie-parser");
-const helmet = require("helmet");
-const morgan = require("morgan");
-
 const app = express();
 
 // Use CORS middleware to allow requests from your frontend
 app.use(cors({
-  origin: ["http://localhost:3000", process.env.WOUESSI_FRONTEND_URL, "https://dev.wouessi.com/en", "https://dev.wouessi.com", "https://www.wouessi.com/en", "https://www.wouessi.com", "https://www.wouessi.ca/en/", "https://www.wouessi.ca"], // Dynamically set the allowed CORS origin
+  origin: [
+    "http://localhost:3000",
+    process.env.WOUESSI_FRONTEND_URL,
+    "https://dev.wouessi.com/en",
+    "https://dev.wouessi.com",
+    "https://www.wouessi.com/en",
+    "https://www.wouessi.com",
+    "https://www.wouessi.ca/en/",
+    "https://www.wouessi.ca"
+  ],
   credentials: true,
 }));
 
 // Middleware
 app.use(express.json());
-
-// Add the newsletter route
-app.use('/api/newsletter', newsletterRoute);
-app.use('/api/blog', blogRoute);
-app.use('/api/contact', contactRoute);
-
-//new 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Routes
+app.use("/api/newsletter", newsletterRoute);
+app.use("/api/blog", blogRoute);
+app.use("/api/contact", contactRoute);
 app.use("/api/auth", authRoutes);
-
-
-// Employee Routes
 app.use("/api/employee", employeeRoutes);
 app.use("/api/department", departmentRoutes);
 app.use("/api/role", roleRoutes);
 app.use("/api/designation", designationRoutes);
 app.use("/api/tasks", taskRoutes);
+app.use("/api/settings", settingRoutes); // 
 
-
+// Connect to MongoDB
 const dbName = "Wouessi";
 
 connectToDB(dbName)
@@ -68,7 +68,7 @@ connectToDB(dbName)
     process.exit(1); // Exit the process if the connection fails
   });
 
-// Define your routes
+// Test route
 app.get('/', (req, res) => {
   res.send('Welcome to Wouessi Back Office');
 });
@@ -82,6 +82,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-
-
